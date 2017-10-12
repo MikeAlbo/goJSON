192c8bd10d8ce24b91ee510ed07266712ce40f51
Michael Albonetti <michaelalbonetti@Michaels-MacBook-Pro.local>
2017-10-12 18:33:03 -0500

    added files to handle UUID creation and prompting user to package main

---
+++ marshallData.go
@@ -0,0 +1,14 @@
+package main
+
+// marshall data from a go into a json file
+
+// generate a user id
+
+// prompt the user for input fields
+
+// construct the struct
+
+// add the data to the db file
+
+// ** simple questions, no validation required -- all inputs are strings**
+
